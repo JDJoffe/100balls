872e0418ddb678e19f90cd5f088879619c358c27
josh <48034855+JDJoffe@users.noreply.github.com>
2019-06-19 13:58:56 +1000

    done

---
+++ 100balls/Assets/Scripts/Bucket.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bucket : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    private Rigidbody2D rigid;
+    private Renderer[] rends;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        rends = GetComponentsInChildren<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandlePosition();
+        HandleBoundary();
+    }
+
+    void HandlePosition()
+    {
+        rigid.velocity = Vector3.left * moveSpeed;
+    }
+
+    void HandleBoundary()
+    {
+        Vector3 transformPos = transform.position;
+
+        Vector3 viewportPos = Camera.main.WorldToViewportPoint(transformPos);
+        if (IsVisible() == false && viewportPos.x < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    bool IsVisible()
+    {
+        foreach ( var renderer in rends)
+        {
+            if (renderer.isVisible)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ 100balls/Assets/Scripts/Paddles.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddles : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            anim.SetBool("IsOpened", true);
+        }
+        else anim.SetBool("IsOpened", false);
+
+    }
+}
